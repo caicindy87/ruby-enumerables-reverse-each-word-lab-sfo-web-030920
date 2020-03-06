c42ae83f0dc0edcdc25870384970b9eb9597f070
Cindy Cai <caicindy87@gmail.com>
2020-03-06 03:16:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,8 @@ require 'pry'
 
 def reverse_each_word(string)
   new_array = string.split
-  new_array.collect do |word|
+  reversed = new_array.collect do |word|
     word.reverse
   end
+  reversed.join(' ')
 end
